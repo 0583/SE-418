b2ad4370630443962e2f743e7bf476cf24f6fbce
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-05-19 18:39:04 +0800

    [add] sub test calling script

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash 
 
+/bin/echo === LOADING TESTS CASES ===
+
+# Run Test Phase 1
+../scripts/test_phase_1.sh
+
+# Run Test Phase 2
+../scripts/test_phase_2.sh
+
+# Run Test Phase 3
+../scripts/test_phase_3.sh
+
+# Completed!
 /bin/echo === ALL TESTS COMPLETED SUCCESSFULLY! ===
