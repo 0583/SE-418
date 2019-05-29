d9748de73c8708f7541764964d9a6ff618ff4c1f
yuetsin <akaza_akari@sjtu.edu.cn>
2019-05-29 11:10:21 +0800

    [add] springboot & kotlin init

---
+++ server/illumi/src/main/kotlin/com/zfet/illumi/IllumiApplication.kt
@@ -0,0 +1,11 @@
+package com.zfet.illumi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IllumiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IllumiApplication>(*args)
+}
