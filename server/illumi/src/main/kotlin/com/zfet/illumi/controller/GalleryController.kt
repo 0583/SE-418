778b871e4a139f3ed80e4570809784fa66bc0425
yuetsin <akaza_akari@sjtu.edu.cn>
2019-05-29 16:25:19 +0800

    [fix] old-fashion annotation style

--- server/illumi/src/main/kotlin/com/zfet/illumi/controller/GalleryController.kt
+++ server/illumi/src/main/kotlin/com/zfet/illumi/controller/GalleryController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(value = "/gallery")
+@RequestMapping(value = ["/gallery"])
 class GalleryController {
     @Autowired
     private lateinit var imageService: ImageService
