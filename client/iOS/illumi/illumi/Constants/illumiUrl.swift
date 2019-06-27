f0d4cfb44749c8cc2a9b07b9bbfa999e707cd6b7
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-27 20:54:36 +0800

    [add] complete login phase

---
+++ client/iOS/illumi/illumi/Constants/illumiUrl.swift
@@ -0,0 +1,13 @@
+//
+//  illumiUrl.swift
+//  illumi
+//
+//  Created by 0583 on 2019/6/27.
+//  Copyright © 2019 0583. All rights reserved.
+//
+
+import Foundation
+
+class illumiUrl {
+    static let loginPostUrl = "http://94.191.27.99:8086/login"
+}
--- client/iOS/illumi/illumi/VC/LoginViewController.swift
+++ client/iOS/illumi/illumi/VC/LoginViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var loginMainView: UIView!
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passWordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         
@@ -20,7 +24,19 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         userNameTextField.delegate = self
         passWordTextField.delegate = self
+        
+        userNameTextField.addTarget(self, action: #selector(checkButtonsValidation(_:)), for: UIControl.Event.editingChanged)
+        passWordTextField.addTarget(self, action: #selector(checkButtonsValidation(_:)), for: UIControl.Event.editingChanged)
+        
+        checkButtonsValidation(userNameTextField)
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        userNameTextField.text = ""
+        passWordTextField.text = ""
+        checkButtonsValidation(userNameTextField)
+        userNameTextField.becomeFirstResponder()
     }
 
 
@@ -39,9 +55,54 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    func goToNextPage() {
+        NSLog("Successfully Logged in!")
+        let destinationStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let destinationViewController = destinationStoryboard.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
+        self.present(destinationViewController, animated: true, completion: nil)
+
+    }
+    
+    func declareRetry(message: String) {
+        let controller = UIAlertController(title: "Failed to Login", message: "The server says “\(message)”. \nCheck your name and password, and try again.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        controller.addAction(okAction)
+        present(controller, animated: true, completion: nil)
+        
+        userNameTextField.isEnabled = true
+        passWordTextField.isEnabled = true
+    }
+    
+    @IBAction func checkButtonsValidation(_ sender: UITextField) {
+        if userNameTextField.text == "" && passWordTextField.text == "" {
+            resetButton.isEnabled = false
+            loginButton.isEnabled = false
+        } else {
+            resetButton.isEnabled = true
+            if userNameTextField.text == "" || passWordTextField.text == "" {
+                loginButton.isEnabled = false
+            } else {
+                loginButton.isEnabled = true
+            }
+        }
+    }
+    
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+        loginTapped()
+    }
+    
+    
     func loginTapped() {
+        if userNameTextField.text == "" || passWordTextField.text == "" {
+            return
+        }
         userNameTextField.isEnabled = false
         passWordTextField.isEnabled = false
+        
+        LoginManager.performLogin(userName: userNameTextField.text!,
+                                  passWord: passWordTextField.text!,
+                                  completionHandler: goToNextPage,
+                                  failureHandler: declareRetry)
     }
     
     /*
