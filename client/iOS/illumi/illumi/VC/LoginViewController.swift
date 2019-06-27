ee11cd1ba92cdb57fc0a50874a325571e5db306b
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-27 17:17:51 +0800

    [submit] partial work sync

--- client/iOS/illumi/illumi/VC/LoginViewController.swift
+++ client/iOS/illumi/illumi/VC/LoginViewController.swift
@@ -10,74 +10,41 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var loginMainView: UIView!
+    
     override func viewDidLoad() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
         super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIKeyboardWillShowNotification, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIKeyboardWillHideNotification, object: nil)
         
         // Do any additional setup after loading the view.
     }
     
-    func keyboardWillShow(note: NSNotification) {
-        let info = note.userInfo
-        let keyboardSize = info
-        let keyboardSize = info.        (UIKeyboardFrameEndUserInfoKey)
-    
-            NSDictionary *info = [note userInfo];
-    
-            CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
-    
-            //目标视图UITextField
-    
-            CGRect frame = passwordTF.frame;
-    
-            int y = frame.origin.y + frame.size.height - (self.view.frame.size.height - keyboardSize.height);
-    
-            NSTimeInterval animationDuration = 0.30f;
-    
-            [UIView beginAnimations:@"ResizeView" context:nil];
-    
-            [UIView setAnimationDuration:animationDuration];
-    
-            if(y > 0)
-    
-            {
-    
-                    self.view.frame = CGRectMake(0, -y, self.view.frame.size.width, self.view.frame.size.height);
-    
-            }
-    
-            [UIView commitAnimations];
-    
+    @objc func keyboardWillShow(note: NSNotification) {
+        let info: NSDictionary = note.userInfo! as NSDictionary
+        let keyboardSize: CGSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect).size
+        let frame: CGRect = loginMainView.frame
+        let y = frame.origin.y + frame.size.height - self.view.frame.size.height - keyboardSize.height
+//        let animationDuration: TimeInterval = 0.30
+//        UIView.beginAnimations("ResizeView", context: nil)
+//        UIView.setAnimationDuration(animationDuration)
+        if y > 0 {
+            self.view.frame = CGRect(x: 0, y: -y, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        }
+//        UIView.commitAnimations()
     }
     
-    
-    
-    //键盘隐藏后将视图恢复到原始状态
-    
-    -(void)keyboardWillHide:(NSNotification *)note
-    
-    {
-    
-            NSTimeInterval animationDuration = 0.30f;
-    
-            [UIView beginAnimations:@"ResizeView" context:nil];
-    
-            [UIView setAnimationDuration:animationDuration];
-    
-            self.view.frame =CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    
-            [UIView commitAnimations];
-    
+    @objc func keyboardWillHide(note: NSNotification) {
+//        let animationDuration: TimeInterval = 0.30
+//        UIView.beginAnimations("ResizeView", context: nil)
+//        UIView.setAnimationDuration(animationDuration)
+        self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+//        UIView.commitAnimations()
     }
-    ---------------------
-    作者：feiyue0823
-    来源：CSDN
-    原文：https://blog.csdn.net/feiyue0823/article/details/50479364
-    版权声明：本文为博主原创文章，转载请附上博文链接！
-    
+
     @IBAction func UITapped(_ sender: UITapGestureRecognizer) {
         // Objective-C style
         // [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil]
